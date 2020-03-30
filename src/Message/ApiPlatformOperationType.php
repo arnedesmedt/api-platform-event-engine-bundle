2bcdaa624fbe8496b94c56efaeedd27d3b1cc4d1
Arne De Smedt <arnedesmdt@gmail.com>
2020-03-30 13:38:16 +0200

    add all to get collection

--- src/Message/ApiPlatformOperationType.php
+++ src/Message/ApiPlatformOperationType.php
@@ -12,7 +12,7 @@ trait ApiPlatformOperationType
     public static function operationType() : ?string
     {
         switch (true) {
-            case preg_match('/(Create|Add|GetAll)/', self::shortName()):
+            case preg_match('/(Create|Add|GetAll|All)/', self::shortName()):
                 return Type::COLLECTION;
             case preg_match('/(Update|Get|Change|Delete|Remove)/', self::shortName()):
                 return Type::ITEM;
