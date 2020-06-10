61772905aa10790d166c924ea8e36fc390091f18
Arne De Smedt <arnedesmdt@gmail.com>
2020-06-10 15:14:18 +0200

    deserialize delete calls

---
+++ src/EventSubscriber/DeleteDeserializeSubscriber.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ADS\Bundle\ApiPlatformEventEngineBundle\EventSubscriber;
+
+use ApiPlatform\Core\EventListener\EventPriorities;
+use ApiPlatform\Core\Serializer\SerializerContextBuilderInterface;
+use ApiPlatform\Core\Util\RequestAttributesExtractor;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Serializer\SerializerInterface;
+
+final class DeleteDeserializeSubscriber implements EventSubscriberInterface
+{
+    private SerializerInterface $serializer;
+    private SerializerContextBuilderInterface $serializerContextBuilder;
+
+    public function __construct(
+        SerializerInterface $serializer,
+        SerializerContextBuilderInterface $serializerContextBuilder
+    ) {
+        $this->serializer = $serializer;
+        $this->serializerContextBuilder = $serializerContextBuilder;
+    }
+
+    /**
+     * @return array<string, array<mixed>>
+     */
+    public static function getSubscribedEvents() : array
+    {
+        return [
+            KernelEvents::REQUEST => ['deserializeForDelete', EventPriorities::POST_DESERIALIZE],
+        ];
+    }
+
+    public function deserializeForDelete(RequestEvent $requestEvent) : void
+    {
+        $request = $requestEvent->getRequest();
+        $method = $request->getMethod();
+
+        if ($method !== Request::METHOD_DELETE) {
+            return;
+        }
+
+        $data = $request->attributes->get('data');
+
+        if ($data !== null) {
+            return;
+        }
+
+        $attributes = RequestAttributesExtractor::extractAttributes($request);
+        $context = $this->serializerContextBuilder->createFromRequest($request, false, $attributes);
+
+        $request->attributes->set(
+            'data',
+            $this->serializer->deserialize(
+                $request->getContent(),
+                $context['resource_class'],
+                'json',
+                $context
+            )
+        );
+    }
+}
--- src/ResourceMetadataFactory/MessageResourceMetadataFactory.php
+++ src/ResourceMetadataFactory/MessageResourceMetadataFactory.php
@@ -6,14 +6,24 @@ namespace ADS\Bundle\ApiPlatformEventEngineBundle\ResourceMetadataFactory;
 
 use ADS\Bundle\ApiPlatformEventEngineBundle\ApiResource\ChangeApiResource;
 use ADS\Bundle\ApiPlatformEventEngineBundle\Config;
+use ADS\Bundle\ApiPlatformEventEngineBundle\Message\ApiPlatformMessage;
 use ApiPlatform\Core\Api\OperationType;
 use ApiPlatform\Core\Metadata\Resource\Factory\ResourceMetadataFactoryInterface;
 use ApiPlatform\Core\Metadata\Resource\ResourceMetadata;
 use phpDocumentor\Reflection\DocBlockFactory;
 use ReflectionClass;
+use Symfony\Component\HttpFoundation\Request;
+use function in_array;
 
 final class MessageResourceMetadataFactory implements ResourceMetadataFactoryInterface
 {
+    private const COMMAND_METHODS = [
+        Request::METHOD_POST,
+        Request::METHOD_DELETE,
+        Request::METHOD_PUT,
+        Request::METHOD_PATCH,
+    ];
+
     private ResourceMetadataFactoryInterface $decorated;
 
     /** @var array<mixed> */
@@ -53,12 +63,12 @@ final class MessageResourceMetadataFactory implements ResourceMetadataFactoryInt
         }
 
         if ($collectionOperations) {
-            $collectionOperations = $this->extractMessageDoc($collectionOperations, $resourceClass, OperationType::COLLECTION);
+            $collectionOperations = $this->handleMessageOperations($collectionOperations, $resourceClass, OperationType::COLLECTION);
             $resourceMetadata = $resourceMetadata->withCollectionOperations($collectionOperations);
         }
 
         if ($itemOperations) {
-            $itemOperations = $this->extractMessageDoc($itemOperations, $resourceClass, OperationType::ITEM);
+            $itemOperations = $this->handleMessageOperations($itemOperations, $resourceClass, OperationType::ITEM);
             $resourceMetadata = $resourceMetadata->withItemOperations($itemOperations);
         }
 
@@ -70,22 +80,46 @@ final class MessageResourceMetadataFactory implements ResourceMetadataFactoryInt
      *
      * @return array<string, array<mixed>>
      */
-    private function extractMessageDoc(array $operations, string $entity, string $operationType) : array
+    private function handleMessageOperations(array $operations, string $entity, string $operationType) : array
     {
         $newOperations = [];
 
         foreach ($operations as $operationName => $operation) {
-            if ($this->mapping[$entity][$operationType][$operationName] ?? false) {
-                $reflectionClass = new ReflectionClass($this->mapping[$entity][$operationType][$operationName]);
+            /** @var class-string<ApiPlatformMessage> $messageClass */
+            $messageClass = $this->mapping[$entity][$operationType][$operationName];
+            if ($messageClass ?? false) {
+                $reflectionClass = new ReflectionClass($messageClass);
 
-                $docBlock = $this->docBlockFactory->create($reflectionClass);
-                $operation['openapi_context']['summary'] = $docBlock->getSummary();
-                $operation['openapi_context']['description'] = $docBlock->getDescription()->render();
+                $this->addDocumentation($operation, $reflectionClass);
+                $this->needRead($operation);
             }
 
             $newOperations[$operationName] = $operation;
         }
 
         return $newOperations;
+    }
+
+    /**
+     * @param array<mixed> $operation
+     * @param ReflectionClass<ApiPlatformMessage> $reflectionClass
+     */
+    private function addDocumentation(array &$operation, ReflectionClass $reflectionClass) : void
+    {
+        $docBlock = $this->docBlockFactory->create($reflectionClass);
+        $operation['openapi_context']['summary'] = $docBlock->getSummary();
+        $operation['openapi_context']['description'] = $docBlock->getDescription()->render();
+    }
+
+    /**
+     * @param array<mixed> $operation
+     */
+    private function needRead(array &$operation) : void
+    {
+        if (! in_array($operation['method'], self::COMMAND_METHODS)) {
+            return;
+        }
+
+        $operation['read'] = false;
     }
 }
