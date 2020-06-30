11c7663af2e21447a8ad77c6325f3df758aafec9
Arne De Smedt <arnedesmdt@gmail.com>
2020-06-30 15:57:35 +0200

    update phpro grumphp and phpstan + shortname feature

--- src/ResourceMetadataFactory/ShortNameResourceMetadataFactory.php
+++ src/ResourceMetadataFactory/ShortNameResourceMetadataFactory.php
@@ -4,17 +4,20 @@ declare(strict_types=1);
 
 namespace ADS\Bundle\ApiPlatformEventEngineBundle\ResourceMetadataFactory;
 
-use ADS\Bundle\EventEngineBundle\Util\EventEngineUtil;
 use ApiPlatform\Core\Metadata\Resource\Factory\ResourceMetadataFactoryInterface;
 use ApiPlatform\Core\Metadata\Resource\ResourceMetadata;
-use ReflectionClass;
-use ReflectionException;
-use RuntimeException;
+use function array_filter;
+use function array_map;
 use function array_search;
 use function explode;
 
 final class ShortNameResourceMetadataFactory implements ResourceMetadataFactoryInterface
 {
+    public const PREFIX_NAMES = [
+        'Entity',
+        'Model',
+    ];
+
     private ResourceMetadataFactoryInterface $decorated;
 
     public function __construct(ResourceMetadataFactoryInterface $decorated)
@@ -33,23 +36,34 @@ final class ShortNameResourceMetadataFactory implements ResourceMetadataFactoryI
             return $resourceMetadata;
         }
 
+        // First try to find the short name by the class
+        // Classes like *\Entity\Bank\* or *\Model\Bank\* will result in short name: Bank.
         $parts = explode('\\', $resourceClass);
-        /** @var int $position */
-        $position = array_search('Entity', $parts);
-        $shortName = $parts[$position + 1] ?? null;
+        $positions = array_filter(
+            array_map(
+                static function (string $prefix) use ($parts) {
+                    $position = array_search($prefix, $parts);
 
-        if ($shortName) {
-            return $resourceMetadata->withShortName($shortName);
-        }
+                    if ($position === false) {
+                        return null;
+                    }
 
-        try {
-            $aggregateRootClass = EventEngineUtil::fromStateToAggregateClass($resourceClass);
-        } catch (RuntimeException | ReflectionException $e) {
-            return $resourceMetadata;
-        }
-
-        return $resourceMetadata->withShortName(
-            (new ReflectionClass($aggregateRootClass))->getShortName()
+                    return $position;
+                },
+                self::PREFIX_NAMES
+            )
         );
+
+        if (! empty($positions)) {
+            foreach ($positions as $position) {
+                $shortName = $parts[$position + 1] ?? null;
+
+                if ($shortName) {
+                    return $resourceMetadata->withShortName($shortName);
+                }
+            }
+        }
+
+        return $resourceMetadata;
     }
 }
