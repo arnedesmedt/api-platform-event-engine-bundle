39454119113e09939b46e5c0577582234b7121fc
Arne De Smedt <arnedesmdt@gmail.com>
2020-07-08 11:00:53 +0200

    no merge with keys

--- src/Serializer/DocumentationNormalizer.php
+++ src/Serializer/DocumentationNormalizer.php
@@ -338,22 +338,20 @@ final class DocumentationNormalizer implements NormalizerInterface
                     ],
                 ];
             },
-            array_merge(
-                array_filter(
-                    [
-                        Response::HTTP_BAD_REQUEST => $schema === null ? null : ApiPlatformException::badRequest(),
-                        Response::HTTP_UNAUTHORIZED => $reflectionClass
-                            ->implementsInterface(AuthorizationMessage::class)
-                            ? ApiPlatformException::unauthorized()
-                            : null,
-                        Response::HTTP_FORBIDDEN => $reflectionClass
-                            ->implementsInterface(AuthorizationMessage::class)
-                            ? ApiPlatformException::forbidden()
-                            : null,
-                    ]
-                ),
-                $messageClass::__responseSchemasPerStatusCode()
-            )
+            $messageClass::__responseSchemasPerStatusCode() +
+            array_filter(
+                [
+                    Response::HTTP_BAD_REQUEST => $schema === null ? null : ApiPlatformException::badRequest(),
+                    Response::HTTP_UNAUTHORIZED => $reflectionClass
+                        ->implementsInterface(AuthorizationMessage::class)
+                        ? ApiPlatformException::unauthorized()
+                        : null,
+                    Response::HTTP_FORBIDDEN => $reflectionClass
+                        ->implementsInterface(AuthorizationMessage::class)
+                        ? ApiPlatformException::forbidden()
+                        : null,
+                ]
+            ),
         );
     }
 
