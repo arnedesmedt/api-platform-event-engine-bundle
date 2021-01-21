723d5763b04f0d91f2da367ec9032e18a90f586e
Arne De Smedt <arnedesmdt@gmail.com>
2021-01-21 15:04:41 +0100

    trim complex type

--- src/PropertyExtractor/PropertyStateExtractor.php
+++ src/PropertyExtractor/PropertyStateExtractor.php
@@ -26,6 +26,7 @@ use function class_exists;
 use function in_array;
 use function is_array;
 use function sprintf;
+use function trim;
 
 final class PropertyStateExtractor implements PropertyListExtractorInterface, PropertyTypeExtractorInterface
 {
@@ -148,6 +149,8 @@ final class PropertyStateExtractor implements PropertyListExtractorInterface, Pr
 
     private static function convertTypeIfComplex(string $type): string
     {
+        $type = trim($type);
+
         if (! class_exists($type)) {
             return $type;
         }
