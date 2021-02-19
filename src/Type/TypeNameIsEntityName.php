9ae625d8fe3cbaa16d8a3d3fd3ec4f30e441fecb
Arne De Smedt <arnedesmdt@gmail.com>
2021-02-19 08:32:04 +0100

    use json schema logic for type traits

--- src/Type/TypeNameIsEntityName.php
+++ src/Type/TypeNameIsEntityName.php
@@ -5,9 +5,12 @@ declare(strict_types=1);
 namespace ADS\Bundle\ApiPlatformEventEngineBundle\Type;
 
 use ADS\Bundle\ApiPlatformEventEngineBundle\Util\Util;
+use ADS\JsonImmutableObjects\JsonSchemaAwareRecordLogic;
 
 trait TypeNameIsEntityName
 {
+    use JsonSchemaAwareRecordLogic;
+
     public static function __type(): string
     {
         return Util::entityNameFromClassName(static::class);
--- src/Type/TypeNameIsEntityNameAndClassName.php
+++ src/Type/TypeNameIsEntityNameAndClassName.php
@@ -5,10 +5,13 @@ declare(strict_types=1);
 namespace ADS\Bundle\ApiPlatformEventEngineBundle\Type;
 
 use ADS\Bundle\ApiPlatformEventEngineBundle\Util\Util;
+use ADS\JsonImmutableObjects\JsonSchemaAwareRecordLogic;
 use ReflectionClass;
 
 trait TypeNameIsEntityNameAndClassName
 {
+    use JsonSchemaAwareRecordLogic;
+
     public static function __type(): string
     {
         $reflectionClass = new ReflectionClass(static::class);
