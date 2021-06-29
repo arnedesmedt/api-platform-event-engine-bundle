ade1e718d700df3071d98d78ffb7b8ceae74a8c5
Arne De Smedt <arnedesmdt@gmail.com>
2021-06-29 16:09:24 +0200

    improve message schema factory

--- src/SchemaFactory/MessageSchemaFactory.php
+++ src/SchemaFactory/MessageSchemaFactory.php
@@ -6,35 +6,39 @@ namespace ADS\Bundle\ApiPlatformEventEngineBundle\SchemaFactory;
 
 use ADS\Bundle\ApiPlatformEventEngineBundle\Exception\FinderException;
 use ADS\Bundle\ApiPlatformEventEngineBundle\Message\Finder;
+use ADS\Bundle\EventEngineBundle\Config;
+use ADS\Bundle\EventEngineBundle\Exception\ResponseException;
 use ADS\Bundle\EventEngineBundle\Response\HasResponses;
 use ApiPlatform\Core\JsonSchema\Schema;
 use ApiPlatform\Core\JsonSchema\SchemaFactoryInterface;
-use ApiPlatform\Core\Metadata\Resource\Factory\ResourceMetadataFactoryInterface;
 use EventEngine\EventEngine;
 use EventEngine\JsonSchema\JsonSchemaAwareRecord;
+use EventEngine\Schema\TypeSchema;
 use ReflectionClass;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
+use RuntimeException;
 
+use function array_flip;
 use function array_merge_recursive;
+use function array_values;
+use function is_callable;
 use function sprintf;
 
 final class MessageSchemaFactory implements SchemaFactoryInterface
 {
     private SchemaFactoryInterface $schemaFactory;
     private Finder $messageFinder;
-    private ResourceMetadataFactoryInterface $resourceMetaDataFactory;
+    private Config $config;
     private EventEngine $eventEngine;
 
     public function __construct(
         SchemaFactoryInterface $schemaFactory,
         Finder $messageFinder,
-        ResourceMetadataFactoryInterface $resourceMetaDataFactory,
+        Config $config,
         EventEngine $eventEngine
     ) {
         $this->schemaFactory = $schemaFactory;
         $this->messageFinder = $messageFinder;
-        $this->resourceMetaDataFactory = $resourceMetaDataFactory;
+        $this->config = $config;
         $this->eventEngine = $eventEngine;
     }
 
@@ -80,25 +84,32 @@ final class MessageSchemaFactory implements SchemaFactoryInterface
         $schema = new Schema(Schema::VERSION_OPENAPI);
         $schema->setDefinitions($definitions);
 
-        $openApiSchema = $type === Schema::TYPE_INPUT
-            ? $message::__schema()
-            : $message::__responseSchemaForStatusCode(
-                $message::__defaultStatusCode() ?? $this->defaultStatusCode($className, $operationType, $operationName)
-            );
+        $openApiSchema = $this->openApiSchema($type, $message, $serializerContext);
 
         $schemaArray = $schema->getArrayCopy();
         $openApiSchemaArray = $openApiSchema->toArray();
 
-        $mergedSchemaArray = array_merge_recursive(
+        $schemaArray = array_merge_recursive(
             $schemaArray,
             $openApiSchemaArray
         );
 
-        $schema->exchangeArray(OpenApiSchemaFactory::toOpenApiSchema($mergedSchemaArray));
+        if ($serializerContext['allowed_properties'] ?? false) {
+            $schemaArray = self::filterParameters(
+                $schemaArray,
+                $serializerContext['allowed_properties'] ?? []
+            );
+        }
+
+        if ($schemaArray === null) {
+            return $schema;
+        }
+
+        $schema->exchangeArray(OpenApiSchemaFactory::toOpenApiSchema($schemaArray));
         $definitions = $schema->getDefinitions();
 
         $refs = OpenApiSchemaFactory::findTypeRefs($openApiSchemaArray);
-        $responseTypes = $this->eventEngine->compileCacheableConfig()['responseTypes'];
+        $responseTypes = $this->config->config()['responseTypes'];
 
         foreach ($refs as $ref) {
             if (! $this->eventEngine->isKnownType($ref)) {
@@ -114,13 +125,13 @@ final class MessageSchemaFactory implements SchemaFactoryInterface
     /**
      * @return class-string|null
      */
-    private function message(string $className, ?string $operationType, ?string $operationName): ?string
+    private function message(string $resourceClass, ?string $operationType, ?string $operationName): ?string
     {
         try {
             /** @var class-string $message */
             $message = $this->messageFinder->byContext(
                 [
-                    'resource_class' => $className,
+                    'resource_class' => $resourceClass,
                     'operation_type' => $operationType,
                     sprintf('%s_operation_name', $operationType) => $operationName,
                 ]
@@ -134,19 +145,86 @@ final class MessageSchemaFactory implements SchemaFactoryInterface
         return $reflectionClass->implementsInterface(JsonSchemaAwareRecord::class) ? $message : null;
     }
 
-    private function defaultStatusCode(string $className, string $operationType, string $operationName): int
+    /**
+     * @param array<string, mixed>|null $serializerContext
+     */
+    private function openApiSchema(string $type, string $message, ?array $serializerContext): TypeSchema
     {
-        $resourceMetaData = $this->resourceMetaDataFactory->create($className);
-        $httpMethod = $resourceMetaData->getTypedOperationAttribute($operationType, $operationName, 'method');
-
-        switch ($httpMethod) {
-            case Request::METHOD_POST:
-                return Response::HTTP_CREATED;
-
-            case Request::METHOD_DELETE:
-                return Response::HTTP_NO_CONTENT;
+        if ($type === Schema::TYPE_INPUT) {
+            return $message::__schema();
         }
 
-        return Response::HTTP_OK;
+        if (isset($serializerContext['status_code'])) {
+            if (isset($serializerContext['response_schema'])) {
+                return $serializerContext['response_schema'];
+            }
+
+            try {
+                return $message::__responseSchemaForStatusCode($serializerContext['status_code']);
+            } catch (ResponseException $exception) {
+            }
+        }
+
+        return $message::__defaultResponseSchema();
+    }
+
+    /**
+     * @param array<mixed> $schema
+     * @param array<mixed> $parameters
+     *
+     * @return array<mixed>|null
+     */
+    public static function removeParameters(array $schema, array $parameters): ?array
+    {
+        return self::changeParameters($schema, $parameters);
+    }
+
+    /**
+     * @param array<mixed> $schema
+     * @param array<mixed> $parameters
+     *
+     * @return array<mixed>|null
+     */
+    public static function filterParameters(array $schema, array $parameters): ?array
+    {
+        return self::changeParameters($schema, $parameters, 'array_intersect');
+    }
+
+    /**
+     * @param array<mixed> $schema
+     * @param array<mixed> $parameters
+     *
+     * @return array<mixed>|null
+     */
+    private static function changeParameters(
+        array $schema,
+        array $parameters,
+        string $filterMethod = 'array_diff'
+    ): ?array {
+        $keyFilterMethod = sprintf('%s_key', $filterMethod);
+
+        if (! is_callable($keyFilterMethod) || ! is_callable($filterMethod)) {
+            throw new RuntimeException(
+                sprintf(
+                    'Method \'%s\' or \'%s\' is not callable',
+                    $keyFilterMethod,
+                    $filterMethod
+                )
+            );
+        }
+
+        $schema['properties'] ??= [];
+        $schema['required'] ??= [];
+
+        $filteredSchema = $schema;
+        $filteredSchema['properties'] = $keyFilterMethod($schema['properties'], array_flip($parameters));
+
+        if (empty($filteredSchema['properties'])) {
+            return null;
+        }
+
+        $filteredSchema['required'] = array_values($filterMethod($schema['required'], $parameters));
+
+        return $filteredSchema;
     }
 }
