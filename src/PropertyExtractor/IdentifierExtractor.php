c69c20cc01d36350fa472de35243fabc6a01c049
Arne De Smedt <arnedesmdt@gmail.com>
2021-08-18 10:24:02 +0200

    split identifiers extractor in 2 ways of extracting

--- src/PropertyExtractor/IdentifierExtractor.php
+++ src/PropertyExtractor/IdentifierExtractor.php
@@ -8,6 +8,7 @@ use ADS\Bundle\ApiPlatformEventEngineBundle\Resource\ChangeIdentifierResource;
 use ADS\Util\ArrayUtil;
 use ADS\ValueObjects\ValueObject;
 use ApiPlatform\Core\Api\IdentifiersExtractorInterface;
+use ReflectionClass;
 
 use function array_map;
 
@@ -21,6 +22,8 @@ class IdentifierExtractor implements IdentifiersExtractorInterface
     }
 
     /**
+     * @param class-string $resourceClass
+     *
      * @return array<mixed>
      *
      * @inheritDoc
@@ -29,7 +32,9 @@ class IdentifierExtractor implements IdentifiersExtractorInterface
     {
         $identifiers = $this->identifiersExtractor->getIdentifiersFromResourceClass($resourceClass);
 
-        return ArrayUtil::toSnakeCasedValues($identifiers);
+        $identifiers = $this->changeListOfIdentifiers($identifiers, $resourceClass);
+
+        return $this->snakeCasedIdentifiers($identifiers);
     }
 
     /**
@@ -41,24 +46,91 @@ class IdentifierExtractor implements IdentifiersExtractorInterface
     {
         $identifiers = $this->identifiersExtractor->getIdentifiersFromItem($item);
 
-        if ($item instanceof ChangeIdentifierResource) {
-            $identifierNameMapping = $item::identifierNameMapping();
+        $identifiers = $this->changeMapOfIdentifiers($identifiers, $item);
+        $identifiers = $this->serializeValueObjects($identifiers);
 
-            foreach ($identifierNameMapping as $oldIdentifierName => $newIdentifierName) {
-                if (! isset($identifiers[$oldIdentifierName])) {
-                    continue;
-                }
+        return $this->snakeCasedIdentifierKeys($identifiers);
+    }
 
-                $identifiers[$newIdentifierName] = $identifiers[$oldIdentifierName];
-                unset($identifiers[$oldIdentifierName]);
-            }
+    /**
+     * @param array<int, string> $identifiers
+     * @param class-string $resourceClass
+     *
+     * @return array<int, string>
+     */
+    private function changeListOfIdentifiers(array $identifiers, string $resourceClass): array
+    {
+        $reflectionClass = new ReflectionClass($resourceClass);
+
+        if (! $reflectionClass->implementsInterface(ChangeIdentifierResource::class)) {
+            return $identifiers;
         }
 
-        $identifiers = array_map(
+        $identifierNameMapping = $resourceClass::identifierNameMapping();
+
+        foreach ($identifiers as &$identifier) {
+            $identifier = $identifierNameMapping[$identifier] ?? $identifier;
+        }
+
+        return $identifiers;
+    }
+
+    /**
+     * @param array<string, mixed> $identifiers
+     * @param mixed $resource
+     *
+     * @return array<string, mixed>
+     */
+    private function changeMapOfIdentifiers(array $identifiers, $resource): array
+    {
+        if (! $resource instanceof ChangeIdentifierResource) {
+            return $identifiers;
+        }
+
+        $identifierNameMapping = $resource::identifierNameMapping();
+
+        foreach ($identifierNameMapping as $oldIdentifierName => $newIdentifierName) {
+            if (! isset($identifiers[$oldIdentifierName])) {
+                continue;
+            }
+
+            $identifiers[$newIdentifierName] = $identifiers[$oldIdentifierName];
+            unset($identifiers[$oldIdentifierName]);
+        }
+
+        return $identifiers;
+    }
+
+    /**
+     * @param array<string, mixed> $identifiers
+     *
+     * @return array<string, mixed>
+     */
+    private function serializeValueObjects(array $identifiers): array
+    {
+        return array_map(
             static fn ($identifier) => $identifier instanceof ValueObject ? $identifier->toValue() : $identifier,
             $identifiers
         );
+    }
 
+    /**
+     * @param array<string, mixed> $identifiers
+     *
+     * @return array<mixed>
+     */
+    private function snakeCasedIdentifierKeys(array $identifiers): array
+    {
         return ArrayUtil::toSnakeCasedKeys($identifiers);
+    }
+
+    /**
+     * @param array<int, string> $identifiers
+     *
+     * @return array<string>
+     */
+    private function snakeCasedIdentifiers(array $identifiers): array
+    {
+        return ArrayUtil::toSnakeCasedValues($identifiers);
     }
 }
