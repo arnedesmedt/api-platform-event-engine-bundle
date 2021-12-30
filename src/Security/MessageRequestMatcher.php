988f3ed2e6ba5e18dc2fb077e76dbd8ba8f901b8
Arne De Smedt <arnedesmdt@gmail.com>
2021-12-30 11:50:09 +0100

    add matches to abstract message request matcher

--- src/Security/MessageRequestMatcher.php
+++ src/Security/MessageRequestMatcher.php
@@ -4,11 +4,15 @@ declare(strict_types=1);
 
 namespace ADS\Bundle\ApiPlatformEventEngineBundle\Security;
 
+use ADS\Bundle\ApiPlatformEventEngineBundle\Message\AuthorizationMessage;
 use ADS\Bundle\ApiPlatformEventEngineBundle\Message\Finder;
 use ApiPlatform\Core\Serializer\SerializerContextBuilderInterface;
 use ApiPlatform\Core\Util\RequestAttributesExtractor;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestMatcherInterface;
+
+use function class_implements;
+use function in_array;
 
 abstract class MessageRequestMatcher implements RequestMatcherInterface
 {
@@ -27,5 +31,15 @@ abstract class MessageRequestMatcher implements RequestMatcherInterface
         $context = $this->serializerContextBuilder->createFromRequest($request, true, $attributes);
 
         return $this->finder->byContext($context);
+    }
+
+    public function matches(Request $request): bool
+    {
+        /** @var class-string $message */
+        $message = $this->message($request);
+
+        $interfaces = class_implements($message);
+
+        return $interfaces !== false && in_array(AuthorizationMessage::class, $interfaces);
     }
 }
