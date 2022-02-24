f889686b1cc50ec4849e202678b8fa8c135443ac
Arne De Smedt <arnedesmdt@gmail.com>
2022-02-24 21:56:02 +0100

    use json schema aware

--- src/Type/TypeNameIsEntityName.php
+++ src/Type/TypeNameIsEntityName.php
@@ -4,10 +4,13 @@ declare(strict_types=1);
 
 namespace ADS\Bundle\ApiPlatformEventEngineBundle\Type;
 
+use ADS\JsonImmutableObjects\JsonSchemaAwareRecordLogic;
 use ADS\Util\StringUtil;
 
 trait TypeNameIsEntityName
 {
+    use JsonSchemaAwareRecordLogic;
+
     public static function __type(): string
     {
         return StringUtil::entityNameFromClassName(static::class);
--- src/Type/TypeNameIsEntityNameAndClassName.php
+++ src/Type/TypeNameIsEntityNameAndClassName.php
@@ -4,11 +4,14 @@ declare(strict_types=1);
 
 namespace ADS\Bundle\ApiPlatformEventEngineBundle\Type;
 
+use ADS\JsonImmutableObjects\JsonSchemaAwareRecordLogic;
 use ADS\Util\StringUtil;
 use ReflectionClass;
 
 trait TypeNameIsEntityNameAndClassName
 {
+    use JsonSchemaAwareRecordLogic;
+
     public static function __type(): string
     {
         $reflectionClass = new ReflectionClass(static::class);
