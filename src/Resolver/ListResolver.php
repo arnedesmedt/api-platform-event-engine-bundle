c4ee86933799d2a6110f94e1a99a0071298a4b36
Arne De Smedt <arnedesmdt@gmail.com>
2022-10-12 12:13:49 +0200

    no templates needed for list resolver

--- src/Resolver/ListResolver.php
+++ src/Resolver/ListResolver.php
@@ -5,15 +5,7 @@ declare(strict_types=1);
 namespace ADS\Bundle\ApiPlatformEventEngineBundle\Resolver;
 
 use ADS\Bundle\EventEngineBundle\Resolver\MetaDataResolver;
-use ADS\ValueObjects\Implementation\ListValue\IterableListValue;
-use ADS\ValueObjects\ValueObject;
-use EventEngine\JsonSchema\JsonSchemaAwareRecord;
 
-/**
- * @template TStates of IterableListValue
- * @template TState of JsonSchemaAwareRecord
- * @template TId of ValueObject
- */
 abstract class ListResolver implements MetaDataResolver
 {
     public function __construct(private readonly InMemoryFilterResolver $inMemoryFilterResolver)
