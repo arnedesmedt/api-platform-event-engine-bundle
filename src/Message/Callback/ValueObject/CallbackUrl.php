c782d83e666387d1832b98a52a5fe161d204c46d
Arne De Smedt <arnedesmdt@gmail.com>
2023-03-07 10:07:56 +0100

    update callback url to new value objects package

--- src/Message/Callback/ValueObject/CallbackUrl.php
+++ src/Message/Callback/ValueObject/CallbackUrl.php
@@ -4,21 +4,11 @@ declare(strict_types=1);
 
 namespace ADS\Bundle\ApiPlatformEventEngineBundle\Message\Callback\ValueObject;
 
-use ADS\ValueObjects\HasExamples;
-use ADS\ValueObjects\Implementation\ExamplesLogic;
-use ADS\ValueObjects\Implementation\RulesLogic;
-use ADS\ValueObjects\Implementation\String\StringValue;
-use EventEngine\JsonSchema\ProvidesValidationRules;
+use ADS\ValueObjects\Implementation\String\UrlValue;
 
-final class CallbackUrl extends StringValue implements HasExamples, ProvidesValidationRules
+final class CallbackUrl extends UrlValue
 {
-    use ExamplesLogic;
-    use RulesLogic;
-
-    /**
-     * @inheritDoc
-     */
-    public static function example()
+    public static function example(): static
     {
         return new static($_SERVER['CALLBACK_URL'] ?? 'https://webhook.site');
     }
