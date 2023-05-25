bddcc62dcc6b8590345d526aeae584a6b1b1584b
Arne De Smedt <arnedesmdt@gmail.com>
2023-05-25 11:09:42 +0200

    Add DefaultApiPlatformMessageWithStateResource

---
+++ src/Message/DefaultApiPlatformMessageWithStateResource.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ADS\Bundle\ApiPlatformEventEngineBundle\Message;
+
+use ADS\Util\StringUtil;
+
+use function sprintf;
+
+trait DefaultApiPlatformMessageWithStateResource
+{
+    use DefaultApiPlatformMessage;
+
+    /** @return class-string */
+    public static function __resource(): string
+    {
+        $resourceNamespace = StringUtil::entityNamespaceFromClassName(static::class);
+        $resourceName = StringUtil::entityNameFromClassName(static::class);
+
+        /** @var class-string $stateClass */
+        $stateClass = sprintf('%s\\%s\\State', $resourceNamespace, $resourceName);
+
+        return $stateClass;
+    }
+}
