c26e2189cc3c44e88608fad25a75258a93426f22
Arne De Smedt <arnedesmdt@gmail.com>
2024-05-31 11:16:05 +0200

    Extend CacheOrNullAdapter

--- src/Cache/CacheOrNullAdapter.php
+++ src/Cache/CacheOrNullAdapter.php
@@ -9,18 +9,21 @@ use Psr\Cache\CacheItemInterface;
 use Symfony\Component\Cache\Adapter\AdapterInterface;
 use Symfony\Component\Cache\Adapter\NullAdapter;
 use Symfony\Component\Cache\CacheItem;
+use Symfony\Component\Cache\PruneableInterface;
+use Symfony\Component\Cache\ResettableInterface;
+use Symfony\Contracts\Cache\CacheInterface;
 
-class CacheOrNullAdapter implements AdapterInterface
+class CacheOrNullAdapter implements AdapterInterface, CacheInterface, PruneableInterface, ResettableInterface
 {
     private readonly NullAdapter $nullAdapter;
 
     public function __construct(
-        private readonly AdapterInterface $adapter,
+        private readonly AdapterInterface&CacheInterface $adapter,
     ) {
         $this->nullAdapter = new NullAdapter();
     }
 
-    private function adapter(): AdapterInterface
+    private function adapter(): AdapterInterface&CacheInterface
     {
         return ComplexTypeExtractor::complexTypeWanted()
             ? $this->nullAdapter
@@ -72,5 +75,38 @@ class CacheOrNullAdapter implements AdapterInterface
     public function commit(): bool
     {
         return $this->adapter()->commit();
+    }
+
+    /** @param array<mixed>|null $metadata */
+    public function get(string $key, callable $callback, float|null $beta = null, array|null &$metadata = null): mixed
+    {
+        return $this->adapter()->get($key, $callback, $beta, $metadata);
+    }
+
+    public function delete(string $key): bool
+    {
+        return $this->adapter()->delete($key);
+    }
+
+    public function prune(): bool
+    {
+        $adapter = $this->adapter();
+
+        if ($adapter instanceof PruneableInterface) {
+            return $adapter->prune();
+        }
+
+        return true;
+    }
+
+    public function reset(): void
+    {
+        $adapter = $this->adapter();
+
+        if (! ($adapter instanceof ResettableInterface)) {
+            return;
+        }
+
+        $adapter->reset();
     }
 }
